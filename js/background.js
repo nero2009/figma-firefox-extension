d172cac4a9de02819e8d690e08a09f4ab9961a06
nero adaware <nero2009@rocketmail.com>
2019-01-11 16:02:11 +0100

    initial commit :tada:

---
+++ js/background.js
@@ -0,0 +1,39 @@
+var storage = browser.storage.local,
+    browserAction = browser.browserAction,
+    windows = browser.windows,
+    tabs = browser.tabs;
+
+storage.get('enabled', function (items) {
+    isEnabled = items.enabled;
+    if (isEnabled || isEnabled == undefined)
+        browserAction.setIcon({ 'path': './icons/icon-32.png' });
+    else
+        browserAction.setIcon({ 'path': './icons/icon-32-disabled.png' });
+});
+
+chrome.extension.onMessage.addListener(function (request) {
+    if (request == "enabled")
+        browserAction.setIcon({ 'path': './icons/icon-32.png' });
+    else if (request == "disabled")
+        browserAction.setIcon({ 'path': './icons/icon-32-disabled.png' });
+});
+
+tabs.onCreated.addListener(function (createdTab) {
+    tabs.query({ windowId: windows.WINDOW_ID_CURRENT }, function (openTabs) {
+        openTabs.forEach(tab => {
+            if (tab.id === createdTab.id) {
+                let url = tab.url,
+                    // Files
+                    figmaFileURL = 'https://www.figma.com/file',
+                    isFigmaFile = url.indexOf(figmaFileURL) == 0,
+                    // Protos
+                    figmaProtoURL = 'https://www.figma.com/proto',
+                    isFigmaProto = url.indexOf(figmaProtoURL) == 0;
+
+                if (isFigmaFile || isFigmaProto) {
+                    tabs.update(tab.id, { active: true });
+                }
+            }
+        });
+    });
+})
---
+++ js/popup.js
@@ -0,0 +1,14 @@
+var storage = browser.storage.local;
+
+storage.get('enabled', function(items) {
+    isEnabled = items.enabled;
+    if (isEnabled || isEnabled == undefined)
+        storage.set({ 'enabled': false }, updateExtensionStatus("disabled"));
+    else
+        storage.set({ 'enabled': true }, updateExtensionStatus("enabled"));
+});
+
+function updateExtensionStatus(status) {
+    chrome.extension.sendMessage(status);
+    window.close();
+}
